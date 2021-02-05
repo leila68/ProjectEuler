8b5163f720d614d17cbf7ff6e165f3c792494dd1
DESKTOP-DARSAH7\Leila <leila.cheshmi@gmail.com>
2021-02-05 21:09:38 +0330

    updated multiple.cs

---
+++ Problem05/multiple.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smallest_multiple
+{
+    class multiple
+    {
+        int m=10;
+        int count;
+        public int finding()
+        {
+            do
+            {
+                for (int i = 1; i <= 20; i++)
+                {
+                    if (m % i == 0)
+                    {
+                        count++;
+                    }
+                    else
+                        break;
+                }
+
+                if (count != 20)
+                {
+                    count = 0;
+                    m++;
+                }
+
+            } while (count < 20);
+
+
+         return m;
+        }
+        public void print()
+        {
+            Console.WriteLine("the smallest number that can be divided by each of the numbers from 1 to 20 without any remainder is:{0}",m);
+        }
+    }
+}
