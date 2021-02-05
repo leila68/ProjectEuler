3aefd76b53e7ea72aae144909d8b7575f32bdb2c
DESKTOP-DARSAH7\Leila <leila.cheshmi@gmail.com>
2021-02-05 21:04:01 +0330

    updated Program.cs

---
+++ Problem05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smallest_multiple
+{
+    class Program
+    {
+         
+          
+        static void Main(string[] args)
+        {
+        
+            multiple obj1 = new multiple();
+            obj1.finding();
+            obj1.print();
+            Console.ReadKey();
+        }
+    }
+}
