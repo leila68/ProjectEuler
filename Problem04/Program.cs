b5c21119be6e65cee07377271b82d39afb9b8100
DESKTOP-DARSAH7\Leila <leila.cheshmi@gmail.com>
2021-02-05 20:53:11 +0330

    updated Program.cs

---
+++ Problem04/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace palindrom
+{ 
+   
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+      
+            Palindrom obj1 = new Palindrom();
+            obj1.palin();
+            Console.ReadKey();
+           
+        }
+    
+    
+    }
+}
