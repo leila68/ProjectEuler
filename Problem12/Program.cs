5249171cbecbb7c3d66125633659bd8cef919f33
DESKTOP-DARSAH7\Leila <leila.cheshmi@gmail.com>
2021-02-06 15:55:27 +0330

    updated Program.cs

---
+++ Problem12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            triangleNumber obj1 = new triangleNumber();
+            
+               obj1.triangle();
+            // int a = obj1.divisor(28);
+        // long a =  obj1.primeDivisor(600851475143); problem3 
+            Console.ReadLine();
+        }
+    }
+}
