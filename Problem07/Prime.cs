904dc83fad77ba8bb3c6f98f04f178c953ec5479
DESKTOP-DARSAH7\Leila <leila.cheshmi@gmail.com>
2021-02-06 15:32:59 +0330

    updated Prime.cs

---
+++ Problem07/Prime.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace problem07
+{
+    class Prime
+    {
+        public bool isPrime(int numm)
+        {
+            if (numm <= 1)
+            {
+                return false;
+            }
+
+            if (numm == 2)
+            {
+                return true;
+            }
+
+            if (numm % 2 == 0)
+            {
+                return false;
+            }
+
+            int counter = 3;
+
+            while ((counter * counter) <= numm)
+            {
+                if (numm % counter == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    counter += 2;
+                }
+            }
+
+            return true;
+        }
+    }
+}
