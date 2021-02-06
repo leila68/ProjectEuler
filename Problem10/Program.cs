04ed54accf99be029e19e3cdc51a482b742c73f3
DESKTOP-DARSAH7\Leila <leila.cheshmi@gmail.com>
2021-02-06 15:40:49 +0330

    updated Program.cs

---
+++ Problem10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace problem07
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Prime obj1 = new Prime();
+            Prime2 obj2 = new Prime2();
+            double s = obj2.total();
+            
+            int numPrim=0;
+            int num=0;
+
+            do
+            {
+                num++;
+                if (obj1.isPrime(num))
+                {
+                    numPrim++;
+                }
+                
+            } while (numPrim < 10001);
+
+            Console.WriteLine("10001st prime is:{0}", num);
+            Console.WriteLine(s);
+
+           
+
+            Console.ReadKey();
+        }
+    }
+}
