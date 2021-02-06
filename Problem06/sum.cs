3177b247dd507d6b58b1cc4c00fe0a7b40690dd2
DESKTOP-DARSAH7\Leila <leila.cheshmi@gmail.com>
2021-02-06 14:14:25 +0330

    updated sum.cs

---
+++ Problem06/sum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace problem6
+{
+    class sum
+    {
+        int sub;
+        public int sumsquare()
+        {
+            int sum1=0;
+            int sum2=0;
+            
+            for (int i=0; i<=100; i++)
+            {
+                sum1 = i * i + sum1;
+                sum2 = i + sum2;
+            }
+
+            sub = (sum2 * sum2) - sum1;
+            return sub;
+        }
+        public void print()
+        {
+            Console.WriteLine("the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is:{0}",sub);
+        }
+    }
+}
