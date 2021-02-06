10037e63a9d2f638602696852a8738d57f6c6fbc
DESKTOP-DARSAH7\Leila <leila.cheshmi@gmail.com>
2021-02-06 15:39:42 +0330

    updated triplet.cs

---
+++ Problem09/triplet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace problem9
+{
+    class triplet
+    {
+        int a,b,c;
+        int result = 3;
+        public void abc()
+        {
+            for (int i = 1; i < 1000; i++)
+                for (int j = i+1; j < 1000; j++)
+                    for (int k = i+2; k < 1000; k++)
+                    {
+                        if ((i*i) + (j*j)  == k*k)
+                        {
+                            if (i+j+k==1000)
+                            {
+                                Console.WriteLine("{0} {1} {2}", i, j, k);
+                                Console.WriteLine(i * j * k);
+                            }
+                            break;
+                        }
+                            
+                        
+                    }
+        }
+    }
+}
