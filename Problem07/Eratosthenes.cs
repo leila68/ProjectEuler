372e4657bfc8d165df3c9df979c54bec64b0dddd
DESKTOP-DARSAH7\Leila <leila.cheshmi@gmail.com>
2021-02-06 15:33:49 +0330

    updated Eratosthenes.cs

---
+++ Problem07/Eratosthenes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace problem07
+{
+    class Eratosthenes
+    {
+
+        public bool isPrime(int num)
+        {
+            int n = num;
+            double s = Math.Sqrt(n);
+             if (n <= 1)
+                 {
+                   return false;
+                 }
+            if (n == 2)
+               {
+                  return true;
+               }
+
+            if (n % 2 == 0)
+                {
+                    return false;
+                }
+            if (n  == 3)
+            {
+                return true;
+            }
+
+            for (int i = 3; i <= s; i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
