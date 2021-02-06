9322874beb348ffd01fb0676a5d9d862a3f1d6d4
DESKTOP-DARSAH7\Leila <leila.cheshmi@gmail.com>
2021-02-06 15:36:52 +0330

    updated 13multiply.cs

---
+++ Problem08/13multiply.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace problem8
+{
+    class _13multiply
+    {
+        int max = 0;
+        int[] num = new int[13];
+
+    }
+}
