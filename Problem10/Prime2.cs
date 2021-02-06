1bb487cbef3bc627c7ac4c75f2e97bbf7e5a0139
DESKTOP-DARSAH7\Leila <leila.cheshmi@gmail.com>
2021-02-06 15:43:07 +0330

    updated Prime2.cs

---
+++ Problem10/Prime2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace problem07
+{
+    class Prime2:Prime
+    {
+        public double sum = 0;
+        public int num = 0;
+        public int prim = 2;
+        Prime ob = new Prime();
+
+        public double total()
+        {
+            while(prim<2000000)
+            {
+                num++;
+                if (ob.isPrime(num))
+              {
+                    prim = num;
+                    if (prim<2000000)
+                    sum += num;
+                        
+                }
+            } 
+            
+            return sum;  
+        }
+        
+    }
+}
